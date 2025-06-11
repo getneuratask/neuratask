eed7c68c19787bcc0763af3a58ab4980a525439a
Dkuaik <enrq.rios.f@gmail.com>
2025-06-11 16:18:07 -0600

    oruebas de conexión y ejemplo en el home

--- frontend/src/views/HomeView.tsx
+++ frontend/src/views/HomeView.tsx
@@ -1,3 +1,27 @@
+import { useEffect, useState } from "react";
+
 export default function HomeView() {
-  return <div className="bg-red-500 text-4xl p-4 text-white">Prueba de Tailwind</div>;
+  
+  const [users, setUsers] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/v1/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    
+    <>
+      <div className="bg-red-500 text-4xl p-4 text-white">Usuarios desde el back</div>
+      <ul className="mt-4">
+        {users.map((user, idx) => (
+          <li key={user.id || idx} className="p-2 border-b">
+            {JSON.stringify(user)}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
